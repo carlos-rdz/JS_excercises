60a3ec42250fa294ca489098b2c98986f8f40e83
Carlos Rodriguez <carlos.a.rodriguez100@gmail.com>
2018-10-07 11:00:00 -0400

    more practice

--- script/loops.js
+++ script/loops.js
@@ -1,3 +1,15 @@
-for (let count = 0; count < 10; count++) {
-    console.log(count)
+let starting_Number = parseInt(prompt(`Where do you want to start?:`)) ;
+const ending_Number = parseInt(prompt(`Where do you want to end?:`)) ;
+
+
+// console.log(starting_Number);
+// console.log(ending_Number);
+
+while (starting_Number < ending_Number+1) {
+
+    if (starting_Number % 2 !== 0){
+    
+        console.log(`Your number is ${starting_Number}`);
+    } 
+    starting_Number++;
 }
