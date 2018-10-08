799d482d26902c35917d48b8a635b3cd225664b1
Carlos Rodriguez <carlos.a.rodriguez100@gmail.com>
2018-10-08 15:31:49 -0400

    Turned leet into a function

--- script/leet.js
+++ script/leet.js
@@ -1,28 +1,25 @@
 const text = prompt(`What do you want to convert to leet?: `);
+
+
+
+function leet_Speak(a){
+
 const lettersToNumbers = {
     'A':4,'E':3,'G':6,'I':1,'O':0,'S':5,'T':7};
-
-const textToTranslate = text.toUpperCase();
+const textToTranslate = a.toUpperCase();
 let translation = ``;
 
 for (let letter of textToTranslate){
-
     if (lettersToNumbers[letter] !== undefined){
         translation = translation + lettersToNumbers[letter]
     } else {
         translation = translation + letter
     }
-    
-    
-    
-
-
-
 }
 console.log(translation)
 
-
-
+}
+leet_Speak(text)
 
 
 // const textToTranslate = text.toUpperCase();
